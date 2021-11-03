9b4b68e40d2f45767e6c8be6b014f9e60e3caa3e
Peter Lamson <ptrlmsn@gmail.com>
2021-11-03 12:25:19 -0400

    commit tree c410a167487d365cc9fdae59305e4b68939ba42a

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+function findMatching (inArray, inName) {
+    const matchingDrivers = inArray.filter (function (name) {
+    return name.toLowerCase() === inName.toLowerCase()
+    })
+    return matchingDrivers
+}
+
+function fuzzyMatch (inArray, inLetters) {
+    const lengthCheck = inLetters.length
+    const matchingDrivers = inArray.filter (function (name) {
+        return name.substring (0, lengthCheck) === inLetters} )
+        return matchingDrivers
+}
+
+function matchName (inArray, inName) {
+    const matchingDrivers = inArray.filter (function(pers) {
+        return pers.name === inName
+    })
+    return matchingDrivers
+}
